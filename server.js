17c1eaa67868fa231a9891b0c3ea69b190c15950
scriptMang <aperalta686@gmail.com>
2026-03-16 15:20:52 -0400

    Add route to get random fun fact

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.get('/', (req, res) => {
     res.send("Welcome to the Homepage.")
 })
 
+app.get('/api/fun-facts', async(req, res)=>{
+    try { 
+        const apiResponse = await axios.get('https://uselessfacts.jsph.pl/api/v2/facts/random');
+        res.json(apiResponse.data);
+    } catch(err) {
+        
+    }
+})
+
 app.listen(port, ()=>{
     console.log(`Server listening on http://localhost:${port}`)
 })
