9c52e10f7cd1ba9fc724b343b2f0f4e9ff4b5bf7
scriptMang <aperalta686@gmail.com>
2026-03-16 13:47:48 -0400

    Add sample root route to test rendering  homepage

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 import express from 'express'
 import axios from 'axios'
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the Homepage.")
+})
+
+app.listen(port, ()=>{
+    console.log(`Server listening on http://localhost:${port}`)
+})
