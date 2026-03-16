abcfd2f7c9ecd77062943e22e8df7324768b9326
scriptMang <aperalta686@gmail.com>
2026-03-16 15:50:49 -0400

    Make the json sent by the fun-fact route only return the data about the random fact

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.get('/', (req, res) => {
 app.get('/api/fun-facts', async(req, res)=>{
     try { 
         const apiResponse = await axios.get('https://uselessfacts.jsph.pl/api/v2/facts/random');
-        res.json(apiResponse.data);
+        const randomFact = {"fact": apiResponse.data["text"]};
+        res.json(randomFact);
     } catch(err) {
         console.error('Failed to fetch fun fact:', err.message);
         res.status(500).json({"error": "Could not fetch fun fact"})
